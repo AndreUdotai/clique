f13a22361df87fd13d8bbd48ac21a92c98e5cfcc
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-11 17:43:59 +0100

    added started codes for postController

---
+++ controllers/postController.js
@@ -0,0 +1,26 @@
+const Post = require('../models/Post');
+
+// Display list of all posts.
+exports.post_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: Post list');
+};
+
+// Display detail page for a specific Post.
+exports.post_detail = (req, res) => {
+    res.send(`NOT IMPLEMENTED: Post detail: ${req.params.id}`);
+};
+
+// Handle Post create on POST.
+exports.post_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Post create POST');
+};
+
+// Handle Post delete on POST.
+exports.post_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Post delete POST');
+};
+
+// Handle Post update on POST.
+exports.post_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Post update POST');
+};
