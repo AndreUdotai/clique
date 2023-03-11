782e203ba260efe23edeb38f4b395e611b878ddc
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-11 18:07:52 +0100

    added starter codes to userRoute

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Require User controller module
+const user_controller = require('../controllers/userController');
+
+// POST request for creating User.
+router.post("/user/create", user_controller.user_register);
+
+// POST request to delete User.
+router.post("/user/:id/delete", user_controller.user_delete);
+
+// POST request to update User.
+router.post("/user/:id/update", user_controller.user_update);
+
+// GET request for one User.
+router.get("/user/:id", user_controller.user_detail);
+
+// GET request for list of all User items.
+router.get("/users", user_controller.user_list);
 
 module.exports = router;
