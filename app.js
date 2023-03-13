9620f6d257c310cba6d38ce75e8818d99580a77d
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-13 05:48:40 +0100

    added comments router codes, edited the filename  and updated app.js comment routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let postsRouter = require('./routes/posts');
+let commentsRouter = require('./routes/comments');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/api', postsRouter);
+app.use('/api', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
