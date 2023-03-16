4fda6b5eb047b980a823f948707a351e888b9ce5
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-16 21:18:35 +0100

    merged post and comment routes to adhere to REST principles

--- routes/posts.js
+++ routes/posts.js
@@ -3,20 +3,37 @@ var router = express.Router();
 
 // Require Post controller module
 const post_controller = require('../controllers/postController');
+// Require Comment controller module
+const comment_controller = require('../controllers/commentController');
 
 // POST request for creating Post.
 router.post("/post/create", post_controller.post_create);
 
 // POST request to delete Post.
-router.post("/post/:id/delete", post_controller.post_delete);
+router.post("/post/:postid/delete", post_controller.post_delete);
 
 // POST request to update Post.
-router.post("/post/:id/update", post_controller.post_update);
+router.post("/post/:postid/update", post_controller.post_update);
 
 // GET request for one Post.
-router.get("/post/:id", post_controller.post_detail);
+router.get("/post/:postid", post_controller.post_detail);
 
 // GET request for list of all Post items.
 router.get("/posts", post_controller.post_list);
+
+// POST request for creating Comment for a particular Post.
+router.get("/post/:postid/comment/create", comment_controller.comment_create);
+
+// POST request to delete Comment.
+router.post("/post/:postid/comment/:commentid/delete", comment_controller.comment_delete);
+
+// POST request to update Comment.
+router.post("/post/:postid/comment/:commentid/update", comment_controller.comment_update);
+
+// GET request for one Comment. ??
+router.get("/post/:postid/comment/:commentid", comment_controller.comment_detail);
+
+// GET request for list of all Comment items.
+router.get("/post/:postid/comments", comment_controller.comment_list);
 
 module.exports = router;
