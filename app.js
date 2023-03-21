7a9f84d8ed88d45f5af79014d392e36990652c24
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-21 15:42:13 +0100

    added babel to the app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
+import createError from 'http-errors';
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let postsRouter = require('./routes/posts');
-let commentsRouter = require('./routes/comments');
+// let commentsRouter = require('./routes/comments');
 
 var app = express();
 
@@ -37,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/api', postsRouter);
-app.use('/api', commentsRouter);
+// app.use('/api', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
