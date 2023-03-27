7a422d539d5a5410bdceeb24837b54ea77d573b9
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-03-28 00:45:59 +0100

    implemented the jwt authorization and route protection codes

---
+++ passport.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+import User from './models/User';
+
+// const loginCheck = (passport) => {
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            let user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect username!',
+                });
+            }
+            // Match Password
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    // passwords match! log user in
+                    return done(null, user, {
+                        message: 'Logged in successfully!',
+                    });
+                } else {
+                    // passwords do not match!
+                    return done(null, false, {
+                        message: 'Incorrect password',
+                    });
+                }
+            });
+        } catch (err) {
+            console.log(done(err));
+            return done(err);
+        }
+    }),
+);
+// };
+
+// module.exports = { loginCheck };
+
+passport.use(
+    new JWTStrategy(
+        {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'secretKey',
+        },
+        function (jwtPayload, cb) {
+            //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+            return User.findById(jwtPayload.user._id)
+                .then((user) => {
+                    return cb(null, user);
+                })
+                .catch((err) => {
+                    return cb(err);
+                });
+        },
+    ),
+);
