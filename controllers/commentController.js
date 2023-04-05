825930c7b858f26498f349e2b09f1c5d64529877
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2023-04-05 17:23:20 +0100

    implemented creating comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,26 +1,61 @@
-const Comment = require('../models/Comment');
+import { body, validationResult } from 'express-validator';
+import Comment from '../models/Comment';
 
 // Display list of all Commnents.
-exports.comment_list = (req, res) => {
+export function comment_list(req, res) {
     res.send('NOT IMPLEMENTED: Comment list');
-};
+}
 
 // Display detail page for a specific Comment.
-exports.comment_detail = (req, res) => {
+export function comment_detail(req, res) {
     res.send(`NOT IMPLEMENTED: Comment detail: ${req.params.id}`);
-};
+}
 
 // Handle Comment create on POST.
-exports.comment_create = (req, res) => {
-    res.send('NOT IMPLEMENTED: Comment create POST');
-};
+export const comment_create = [
+    // validate and sanitize fields.
+    body('body', 'You did not enter a comment!')
+        .trim()
+        .isLength({ min: 1 }),
+    
+    // Process request after validation and sanitization.
+    async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Comment Object with trimmed data.
+        const comment = new Comment({
+            body: req.body.body,
+            user: req.user._id,
+            post: req.params.postid,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors.
+            res.status(400).json({
+                message: "You did not enter a comment!",
+                errors: errors,
+            });
+            return;
+        } else {
+            try {
+                await comment.save();
+                res.status(200).json({
+                    message: "Your comment was saved!",
+                });
+            } catch (err) {
+                return next(err);
+            }
+        }
+    }
+]
 
 // Handle Comment delete on POST.
-exports.comment_delete = (req, res) => {
+export function comment_delete(req, res) {
     res.send('NOT IMPLEMENTED: Comment delete POST');
-};
+}
 
 // Handle Comment update on POST.
-exports.comment_update = (req, res) => {
+export function comment_update(req, res) {
     res.send('NOT IMPLEMENTED: Comment update POST');
-};
+}
